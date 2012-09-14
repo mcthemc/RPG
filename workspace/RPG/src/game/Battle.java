2d7f1365416a52facd7f10f6e679b7eeceef82f3
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2012-09-13 23:59:24 -0400

    Getting the changes on the current classes actually committed.

--- workspace/RPG/src/game/Battle.java
+++ workspace/RPG/src/game/Battle.java
@@ -1,5 +1,5 @@
 package game;
 
 public class Battle {
-
+	// summat is going to happen around here. . .
 }
--- workspace/RPG/src/game/Character.java
+++ workspace/RPG/src/game/Character.java
@@ -1,10 +1,12 @@
 package game;
 
 public class Character {
-	public class current { // information about the current state of your character
-		
-		
-	}
-	private class 
+	// character info fields
+
+	// character stats fields
+
+	// character field read methods
+
+	// character field modification methods
 
 }

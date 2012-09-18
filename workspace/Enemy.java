bcecda7624b296f6e42986e5c8dbbc9aceba28ef
DeathRx <zwh2@case.edu>
2012-09-17 21:48:24 -0400

    changed the enemy get methods and constructors to make it more formal and easier to follow

--- workspace/Enemy.java
+++ workspace/Enemy.java
@@ -2,25 +2,30 @@ import java.util.Random;
 
 public class Enemy { 
   /* in the distant future, the Enemy stats and type will be based on location and/or player level. */ 
-  Random rand = new Random(); 
+  private Random rand = new Random(); 
   
   /* STATS */
-  private int level = rand.nextInt(10); // ITDF, this will be set by an if statement, based on the location
-  private int HP = level * 10; // ITDF this will have some sort of equation to set a fair HP based on level and stats
+  private int level;
+  private int HP;
   
-  public int getStats(String stat) { 
-    if (stat.equals("level")) 
-      return level; 
-    else if (stat.equals("HP"))
-      return HP;
-    else return HP;
+  public Enemy(){
+    Enemy (1);// this creates an enemy with level 1 if there is no level passed to the constructor
   }
-  public void lowerHP(int decrement) {
+  public Enemy(int level){
+    level = rand.nextInt(10); // ITDF, this will be set by an if statement, based on the location and play level
+    HP = level * 10;// ITDF, this will be set by an if statement, based on the location
+  }
+  
+  public int getLevel() {return level;}
+  
+  public int getHP() {return HP;}
+  
+  public void damage(int decrement) {
     if (HP - decrement <= 0) 
       HP = 0; 
     else 
       HP -= decrement; 
-  }
-  public void 
+  }  
   
 }
+

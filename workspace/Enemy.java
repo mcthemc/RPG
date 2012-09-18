1026095aac5a797eaaaaada04cec4ac827d972ce
m. c <m.c.perrin.edu@gmail.com>
2012-09-17 22:30:04 -0400

    small formatting changes.

--- workspace/Enemy.java
+++ workspace/Enemy.java
@@ -4,12 +4,12 @@ public class Enemy {
   /* in the distant future, the Enemy stats and type will be based on location and/or player level. */ 
   private Random rand = new Random(); 
   
-  private int level;
-  private int HP;
+  private int level, HP, beardPoints, power, armor;
   
   public Enemy(){
-    Enemy (1);// this creates an enemy with level 1 if there is no level passed to the constructor
+    Enemy(1); // this creates an enemy with level 1 if there is no level passed to the constructor
   }
+  
   public Enemy(int level){
     level = rand.nextInt(10); // ITDF, this will be set by an if statement, based on the location and play level
     HP = level * 10;// ITDF, this will be set by an if statement, based on the location
@@ -25,6 +25,7 @@ public class Enemy {
     else 
       HP -= decrement; 
   }  
+  
   public void die() { // for when you murder the bastard
     
   }
